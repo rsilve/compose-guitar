017a8dfe918585e9ef9d482aa3cb33c17d84b182
Robert Silve <robert@silve.net>
2021-11-14 19:46:21 +0100

    add test

--- src/editor/actions/__tests__/actions.spec.ts
+++ src/editor/actions/__tests__/actions.spec.ts
@@ -1,9 +1,11 @@
 import {expect} from '@open-wc/testing'
 import {register, reset_dispatcher} from "../../../stores/dispatcher";
 import {
+    action_gallery_close,
+    action_gallery_open, action_gallery_remove,
     action_init_app, action_save_as_start, action_save_as_start_and_new,
     action_transpose_change,
-    action_upload_from_gallery,
+    action_upload_from_gallery, GALLERY_CLOSE, GALLERY_OPEN, GALLERY_REMOVE,
     INIT_APP, SAVE_AS_START, SAVE_AS_START_AND_NEW,
     TRANSPOSE_CHANGE,
     UPLOAD_FROM_GALLERY
@@ -68,6 +70,40 @@ suite("actions", () => {
             return Promise.resolve({})
         })
         await action_save_as_start_and_new()
+        expect(handle).to.be.true
+    })
+
+    test("gallery_open", async () => {
+        let handle = false
+        reset_dispatcher(default_state())
+        register(action => {
+            handle = action.action_type === GALLERY_OPEN
+            return Promise.resolve({})
+        })
+        await action_gallery_open()
+        expect(handle).to.be.true
+    })
+
+    test("gallery_gallery_remove", async () => {
+        let handle = 'id'
+        reset_dispatcher(default_state())
+        register(action => {
+            const {id = ''} = action.payload as { id: string }
+            handle = action.action_type === GALLERY_REMOVE ? id : ''
+            return Promise.resolve({})
+        })
+        await action_gallery_remove('id')
+        expect(handle).to.be.equal('id')
+    })
+
+    test("gallery_gallery_close", async () => {
+        let handle = false
+        reset_dispatcher(default_state())
+        register(action => {
+            handle = action.action_type === GALLERY_CLOSE
+            return Promise.resolve({})
+        })
+        await action_gallery_close()
         expect(handle).to.be.true
     })
 
