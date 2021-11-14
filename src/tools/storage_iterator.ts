4eb6afe8d567fd0cec06cbc13ead764d3c9dc2dc
Robert Silve <robert@silve.net>
2021-11-14 11:27:45 +0100

    remove unused file

--- src/tools/storage_iterator.ts
+++
@@ -1,5 +0,0 @@
-export function storage_iterator(): { key: string, value: string | null }[] {
-    return Object.keys(localStorage).map(value => {
-        return {key: value, value: localStorage.getItem(value)}
-    })
-}
