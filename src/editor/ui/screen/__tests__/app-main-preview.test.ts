554266619144d4e4d88f3dc477f03496645d5a30
Robert Silve <robert@silve.net>
2021-11-14 09:19:13 +0100

    fix test

--- src/editor/ui/screen/__tests__/app-main-preview.test.ts
+++ src/editor/ui/screen/__tests__/app-main-preview.test.ts
@@ -1,5 +1,8 @@
 import {expect, fixture, html} from "@open-wc/testing";
 import AppMainPreview from "../app-main-preview";
+import {connect, register, reset_dispatcher} from "../../../../stores/dispatcher";
+import {track_callback} from "../../../stores/register/track";
+import {IState} from "../../../stores/state";
 
 suite("app main preview element", () => {
 
@@ -17,6 +20,24 @@ suite("app main preview element", () => {
                 <p class="no_print">click to edit</p>
             </div>
         `)
+    });
+
+    test('handle edit', async () => {
+        reset_dispatcher()
+        register(track_callback)
+        const promise: Promise<boolean> = new Promise(resolve => {
+            connect((st: IState) => {
+                resolve(!!st.editor)
+            })
+        })
+        const el: AppMainPreview = await fixture(html`
+            <app-main-preview zoom="100" song_title="title" song_grid="A"></app-main-preview> `);
+        expect(el).to.instanceOf(AppMainPreview)
+        const node = el.shadowRoot?.querySelector("div") as HTMLElement
+        node.click()
+        await el.updateComplete
+        await promise.then(value => expect(value).to.be.true)
+
     });
 
 
