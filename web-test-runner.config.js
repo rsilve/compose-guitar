109be4e3a18cea2f774f6d31425554e596c73863
Robert Silve <robert@silve.net>
2021-11-18 20:14:05 +0100

    add support for gitpod

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,9 +1,14 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
     rootDir: '.',
     files: ['./build/**/*.(spec|test).js'],
     nodeResolve: true,
     preserveSymlinks: true,
     coverage: true,
+    browsers: [
+        playwrightLauncher({ product: 'firefox' })
+    ],
     testFramework: {
         // https://mochajs.org/api/mocha
         config: {
