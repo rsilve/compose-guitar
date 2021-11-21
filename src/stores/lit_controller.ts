51fdc9e9285e37115cc4936e2e925b62aea5d82f
Robert Silve <robert@silve.net>
2021-11-21 19:57:56 +0100

    fix incorrect type

--- src/stores/lit_controller.ts
+++ src/stores/lit_controller.ts
@@ -1,11 +1,12 @@
 import {ReactiveController} from 'lit';
 import {connect, disconnect, init} from "./dispatcher";
+import {IState} from "../editor/stores/state";
 
 export class DispatcherController implements ReactiveController {
 
-    private readonly _dispatch_callback: (st: any) => void;
+    private readonly _dispatch_callback: (st: IState) => void;
 
-    constructor(dispatch_callback: (st: any) => void) {
+    constructor(dispatch_callback: (st: IState) => void) {
         this._dispatch_callback = dispatch_callback
     }
 
