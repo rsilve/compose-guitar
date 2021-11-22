6a505176408d7396d4331cc5033c49b76ba5a5d9
Robert Silve <robert@silve.net>
2021-11-22 21:21:34 +0100

    synchronize configuration component has content dependant of synchronization state

--- src/editor/ui/modals/SynchronizeConfiguration.ts
+++ src/editor/ui/modals/SynchronizeConfiguration.ts
@@ -1,9 +1,23 @@
 import {html, LitElement} from 'lit';
-import {customElement} from "lit/decorators.js";
+import {customElement, state} from "lit/decorators.js";
+import {IState} from "../../stores/state";
+import {DispatcherController} from "../../../stores/lit_controller";
 
 
 @customElement('synchronize-configuration')
 class SynchronizeConfiguration extends LitElement {
+
+
+    @state()
+    _enabled = false
+
+    constructor() {
+        super();
+        const cb = (st: IState) => {
+            this._enabled = st.synchronization.enabled
+        }
+        this.addController(new DispatcherController(cb.bind(this)))
+    }
 
     private _dispatch_close() {
         const options = {
@@ -15,7 +29,12 @@ class SynchronizeConfiguration extends LitElement {
 
 
     render(): unknown {
+        let body = html`<div>Do you want to activate synchronization ?</div>`
+        if (this._enabled) {
+            body = html`<div>Do you want to deactivate synchronization ?</div>`
+        }
         return html`
+            ${body}
             <div class="modal-footer">
                 <button tabindex="-1" class="btn-primary _close" @click="${this._dispatch_close}">Close</button>
             </div>
--- src/editor/ui/modals/__tests__/synchronise-configuration.spec.ts
+++ src/editor/ui/modals/__tests__/synchronise-configuration.spec.ts
@@ -1,18 +1,23 @@
 import {expect, fixture, html} from "@open-wc/testing";
 import SynchronizeConfiguration from "../SynchronizeConfiguration";
+import {reset_dispatcher} from "../../../../stores/dispatcher";
+import {state_test} from "../../../../__tests__/TestHelpers";
 
 
 suite("synchronise configuration element", () => {
 
+    const st = state_test
+
     test('is defined', async () => {
+        reset_dispatcher()
         const el: SynchronizeConfiguration = await fixture(html`
             <synchronize-configuration></synchronize-configuration>`);
         expect(el).to.instanceOf(SynchronizeConfiguration)
         await expect(el).shadowDom.to.be.accessible();
     });
 
-
     test('have a close button', async () => {
+        reset_dispatcher()
         let handle_close = false
         const el: SynchronizeConfiguration = await fixture(html`
             <synchronize-configuration @close="${() => handle_close = true}"></synchronize-configuration>`);
@@ -22,6 +27,37 @@ suite("synchronise configuration element", () => {
         expect(node).to.not.be.null
         node.click()
         expect(handle_close).to.be.true
+    });
+
+    test('has default state', async () => {
+        reset_dispatcher()
+        const el: SynchronizeConfiguration = await fixture(html`
+            <synchronize-configuration></synchronize-configuration>`);
+        expect(el).to.instanceOf(SynchronizeConfiguration)
+        await expect(el).shadowDom.to.be.accessible();
+        expect(el._enabled).to.be.false
+        expect(el).shadowDom.to.equals(`
+            <div>Do you want to activate synchronization ?</div>
+            <div class="modal-footer">
+                <button tabindex="-1" class="btn-primary _close">Close</button>
+            </div>
+        `)
+    });
+
+
+    test('listen store', async () => {
+        reset_dispatcher({...st, synchronization: {enabled: true}})
+        const el: SynchronizeConfiguration = await fixture(html`
+            <synchronize-configuration></synchronize-configuration>`);
+        expect(el).to.instanceOf(SynchronizeConfiguration)
+        await expect(el).shadowDom.to.be.accessible();
+        expect(el._enabled).to.be.true
+        expect(el).shadowDom.to.equals(`
+            <div>Do you want to deactivate synchronization ?</div>
+            <div class="modal-footer">
+                <button tabindex="-1" class="btn-primary _close">Close</button>
+            </div>
+        `)
     });
 
 })
