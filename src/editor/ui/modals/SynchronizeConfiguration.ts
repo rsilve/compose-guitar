2b1bb79700016af817e60eb82f5e84e305b759c3
Robert Silve <robert@silve.net>
2021-11-22 20:46:00 +0100

    synchronize configuration component has a close button

--- src/editor/ui/modals/SynchronizeConfiguration.ts
+++ src/editor/ui/modals/SynchronizeConfiguration.ts
@@ -5,8 +5,20 @@ import {customElement} from "lit/decorators.js";
 @customElement('synchronize-configuration')
 class SynchronizeConfiguration extends LitElement {
 
+    private _dispatch_close() {
+        const options = {
+            bubbles: true,
+            composed: true
+        };
+        this.dispatchEvent(new CustomEvent('close', options));
+    }
+
+
     render(): unknown {
         return html`
+            <div class="modal-footer">
+                <button tabindex="-1" class="btn-primary _close" @click="${this._dispatch_close}">Close</button>
+            </div>
         `
     }
 
--- src/editor/ui/modals/__tests__/synchronise-configuration.spec.ts
+++ src/editor/ui/modals/__tests__/synchronise-configuration.spec.ts
@@ -9,7 +9,19 @@ suite("synchronise configuration element", () => {
             <synchronize-configuration></synchronize-configuration>`);
         expect(el).to.instanceOf(SynchronizeConfiguration)
         await expect(el).shadowDom.to.be.accessible();
-        expect(el).shadowDom.to.be.equal(``)
+    });
+
+
+    test('have a close button', async () => {
+        let handle_close = false
+        const el: SynchronizeConfiguration = await fixture(html`
+            <synchronize-configuration @close="${() => handle_close = true}"></synchronize-configuration>`);
+        expect(el).to.instanceOf(SynchronizeConfiguration)
+        await expect(el).shadowDom.to.be.accessible();
+        const node = el.shadowRoot?.querySelector("._close") as HTMLElement
+        expect(node).to.not.be.null
+        node.click()
+        expect(handle_close).to.be.true
     });
 
 })
