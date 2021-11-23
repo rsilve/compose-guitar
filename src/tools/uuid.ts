85e821466695fd36ba4cf0531a39f0749ed2e7fc
Robert Silve <robert@silve.net>
2021-11-23 20:01:01 +0100

    apply eslint fix

--- src/tools/uuid.ts
+++ src/tools/uuid.ts
@@ -7,12 +7,12 @@ export function uuid(): string {
     const array = new Uint8Array(1);
     random = (crypto.getRandomValues(array)[0] | 0) % 16;
 
-    if (i == 8 || i == 12 || i == 16 || i == 20) {
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
       id += '-';
     }
 
-    const num = i == 16 ? (random & 3 | 8) : random;
-    id += (i == 12 ? 4 : num).toString(16);
+    const num = i === 16 ? (random & 3 | 8) : random;
+    id += (i === 12 ? 4 : num).toString(16);
   }
 
   return id;
