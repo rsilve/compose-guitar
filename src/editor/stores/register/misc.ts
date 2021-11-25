969d909db46b242d2e16d4cb5020c7124242e182
Robert Silve <robert@silve.net>
2021-11-25 19:43:36 +0100

    fix lint warning

--- src/editor/stores/register/misc.ts
+++ src/editor/stores/register/misc.ts
@@ -1,41 +1,44 @@
-import { INIT_APP, TRANSPOSE_CHANGE, ZOOM_CHANGE } from "../../actions/actions";
-import { get_last_state } from "./gallery_tools";
-import { IState } from "../state";
+import {INIT_APP, TRANSPOSE_CHANGE, ZOOM_CHANGE} from "../../actions/actions";
+import {get_last_state} from "./gallery_tools";
+import {IState} from "../state";
 import Action from "../../../actions/Action";
 
 export function init_app_callback(action: Action, state: IState): Promise<IState> {
-  if (action.action_type === INIT_APP) {
-    const st = get_last_state();
-    if (st) {
-      state = { ...state, ...st };
+    let result = {...state};
+    if (action.action_type === INIT_APP) {
+        const st = get_last_state();
+        if (st) {
+            result = {...result, ...st};
+        }
     }
-  }
 
-  return Promise.resolve(state);
+    return Promise.resolve(result);
 }
 
 export function zoom_change_callback(action: Action, state: IState): Promise<IState> {
-  if (action.action_type === ZOOM_CHANGE) {
-    let { zoom } = action.payload as { zoom: number };
-    if (isNaN(zoom)) {
-      zoom = 100;
+    let result = {...state};
+    if (action.action_type === ZOOM_CHANGE) {
+        let {zoom} = action.payload as { zoom: number };
+        if (!zoom || Number.isNaN(zoom)) {
+            zoom = 100;
+        }
+        zoom = Math.min(200, zoom);
+        zoom = Math.max(10, zoom);
+        result = {...result, zoom};
     }
-    zoom = Math.min(200, zoom);
-    zoom = Math.max(10, zoom);
-    state = { ...state, zoom };
-  }
-  return Promise.resolve(state);
+    return Promise.resolve(result);
 }
 
 export function transpose_change_callback(action: Action, state: IState): Promise<IState> {
-  if (action.action_type === TRANSPOSE_CHANGE) {
-    let { transpose } = action.payload as { transpose: number };
-    if (isNaN(transpose)) {
-      transpose = 0;
+    let result = {...state};
+    if (action.action_type === TRANSPOSE_CHANGE) {
+        let {transpose} = action.payload as { transpose: number };
+        if (!transpose || Number.isNaN(Number(transpose))) {
+            transpose = 0;
+        }
+        transpose = Math.min(12, transpose);
+        transpose = Math.max(-12, transpose);
+        result = {...result, transpose};
     }
-    transpose = Math.min(12, transpose);
-    transpose = Math.max(-12, transpose);
-    state = { ...state, transpose };
-  }
-  return Promise.resolve(state);
+    return Promise.resolve(result);
 }
