4e2e964f89c7a2a9ef285d44dcc139126d83c332
Robert Silve <robert@silve.net>
2021-11-27 14:40:16 +0100

    add error log

--- src/actions/publish_action.ts
+++ src/actions/publish_action.ts
@@ -4,5 +4,7 @@ import Action from "./Action";
 export function publish_action(action: Action): Promise<void> {
   return dispatch(action).then(() => {
     console.info("action completed", action);
+  }).catch(reason => {
+    console.error("action failed", action, reason);
   });
 }
