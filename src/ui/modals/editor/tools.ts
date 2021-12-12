85f724cb06c574b39aad4e25bb3ea592296ba4f9
Robert Silve <robert@silve.net>
2021-12-12 16:40:11 +0000

    Fix auto correct

--- src/ui/modals/editor/tools.ts
+++ src/ui/modals/editor/tools.ts
@@ -10,7 +10,8 @@ export function auto_correct(value: string): string {
     .replace(/([^|])\n/g, "$1|\n") // | at end of line
     .replace(/\n([^|])/g, "\n|$1") // | at begining of line
     .replace(/\b\.\s*/g, " _ ") // . to insert _
-    .replace(/\b([a-g])/g, (substring) => substring.toUpperCase()) // uppercase note
     .replace(/([^:|\s])(:?)\|/g, "$1 $2|") // space before |
-    .replace(/\|(:?)([^:|\s])/g, "|$1 $2"); // space after |
+    .replace(/\|([a-gA-G])/g, "| $1") // space after |
+    .replace(/\|(\([^)]\))([a-gA-G])/g, "|$1 $2") // space after |(x)
+    .replace(/\s([a-g])/g, (substring) => substring.toUpperCase()); // uppercase note
 }
