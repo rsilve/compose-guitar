e5092c4b29a075f13f0fdaf8dc59f2faeea5f2fb
Robert Silve <robert@silve.net>
2022-02-02 13:39:53 +0100

    add vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+  root: "src",
+  public: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+};
--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,26 +1,28 @@
-import {legacyPlugin} from '@web/dev-server-legacy';
-import rollupReplace from '@rollup/plugin-replace';
-import { fromRollup } from '@web/dev-server-rollup';
-import dotenv from 'dotenv';
+import { legacyPlugin } from "@web/dev-server-legacy";
+import rollupReplace from "@rollup/plugin-replace";
+import { fromRollup } from "@web/dev-server-rollup";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 const replace = fromRollup(rollupReplace);
 
 export default {
-    rootDir: 'build',
-    nodeResolve: true,
-    preserveSymlinks: true,
-    plugins: [
-        replace({ preventAssignment: true, include: ['**/*.js'],
-            __api_key__: process.env.API_KEY,
-            __api_client_id__: process.env.API_CLIENT_ID,
-            __synchro_enabled__: process.env.SYNCHRO_ENABLED,
-        }),
-        legacyPlugin({
-            polyfills: {
-                // Manually imported in index.html file
-                webcomponents: false,
-            },
-        }),
-    ],
+  rootDir: "out",
+  nodeResolve: true,
+  preserveSymlinks: true,
+  plugins: [
+    replace({
+      preventAssignment: true,
+      include: ["**/*.js"],
+      __api_key__: process.env.API_KEY,
+      __api_client_id__: process.env.API_CLIENT_ID,
+      __synchro_enabled__: process.env.SYNCHRO_ENABLED,
+    }),
+    legacyPlugin({
+      polyfills: {
+        // Manually imported in index.html file
+        webcomponents: false,
+      },
+    }),
+  ],
 };
