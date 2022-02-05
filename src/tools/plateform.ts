92a30c9b56619b70f44125a1301cdb742cf9ff6b
Robert Silve <robert@silve.net>
2022-02-05 19:56:43 +0100

    remove unused file

--- src/tools/plateform.ts
+++
@@ -1,9 +0,0 @@
-export const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
-
-export function ctrlOrCommand(e: KeyboardEvent): boolean {
-  return isMac ? e.metaKey : e.ctrlKey;
-}
-
-export function ctrlOrCommendKeyName(): string {
-  return isMac ? "Cmd" : "Ctrl";
-}
