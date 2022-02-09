ef68d3366c06d1e742c9de4334ae3c89d2b584b0
Robert Silve <robert@silve.net>
2022-02-09 06:12:12 +0000

    load locales on page load with navigator language

--- src/editor.ts
+++ src/editor.ts
@@ -2,10 +2,35 @@ import "./ui/editor-main";
 
 import init from "./init";
 import register_callbacks from "./stores/register_callbacks";
+import { html, render } from "lit";
+import { configureLocalization } from "@lit/localize";
+import { sourceLocale, targetLocales, allLocales } from "./generated/locales";
 
 function run(): void {
   register_callbacks();
   init();
 }
 
-run();
+const main = document.querySelector("main")!;
+const { getLocale, setLocale } = configureLocalization({
+  sourceLocale,
+  targetLocales,
+  loadLocale: (locale) => import(`./generated/locales/${locale}`),
+});
+
+(async () => {
+  let lang = window.navigator.language;
+  if (!Object.keys(allLocales).indexOf(lang)) {
+    lang = lang.split("-")[0];
+  }
+  try {
+    await setLocale(lang);
+  } catch (e) {
+    // Either the URL locale code was invalid, or there was a problem loading
+    // the locale module.
+    console.error(`Error loading locale for code '${lang}': ${(e as Error).message}`);
+  }
+  console.info("Language", getLocale());
+  render(html` <editor-main></editor-main> `, main);
+  run();
+})();
--- src/init.ts
+++ src/init.ts
@@ -2,19 +2,9 @@ import { actionInitApp } from "./actions/actions";
 import { connect, initializeState } from "./stores/dispatcher";
 import { saveLastState } from "./stores/register/gallery_tools";
 import { default_state } from "./stores/state";
-import { configureLocalization } from "@lit/localize";
-import { sourceLocale, targetLocales } from "./generated/locales";
-
-export const { getLocale, setLocale } = configureLocalization({
-  sourceLocale,
-  targetLocales,
-  loadLocale: (locale) => import(`./generated/locales/${locale}`),
-});
 
 export default function init(): void {
   initializeState(default_state());
   connect(saveLastState);
   actionInitApp().catch((reason) => console.info("initialization failed", reason));
-
-  setLocale("fr");
 }
