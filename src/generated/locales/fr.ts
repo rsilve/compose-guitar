6b5a142d9e4ad855d7bb762c915a5fd4ca63587b
Robert Silve <robert@silve.net>
2022-02-10 19:30:17 +0000

    add translation

--- src/generated/locales/fr.ts
+++ src/generated/locales/fr.ts
@@ -5,6 +5,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const templates = {
+  s01c9945d94c2d458: `Le message d'erreur était`,
   s2189cdd19d22a843: `Morceau mis à jour`,
   s2b9a1d2a9a3110da: `Séquence d'accords (obligatoire)`,
   s2ba99e6c2f37b21e: `Supprimer de la galerie`,
@@ -14,13 +15,17 @@ export const templates = {
   s559bca2d83795046: `Compléments :`,
   s5e8250fb85d64c23: `Fermer`,
   s614a0ead0aa35590: `activer`,
+  s771bdf9b9dc0dc53: `désactiver`,
   s7bb88acd8dbfaa89: `Ce titre existe déjà`,
   s81ecf2d4386b8e84: `Continuer`,
   s8d349529fc5fb3b7: `Voir les exemples`,
+  sac61352e8ffe9655: `La synchronisation entre les appareils est activée`,
   sb8f855b49234b81b: `Appliquer`,
   sbefcc6bf7b752e55: `Mesures :`,
+  sc1724a8bddde2ed2: `Vous n'êtes pas connecté.`,
   sc27e0098602d39d6: `Exemples`,
   sc4766e4bd5916653: `Syntaxe incorrecte :`,
+  sc4f1c47579ba1771: `Vous êtes connecté`,
   sc8e99647d23486da: `Sauvegarder et continuer`,
   scc854eb62c822760: `Cliquez pour modifier`,
   sd2052cf3f218e095: `Certains changements n'ont pas été enregistrés. Voulez-vous quand même continuer ?`,
@@ -29,4 +34,5 @@ export const templates = {
   sf18cfde2dfadbbbb: `Synchronisation`,
   sf7037ffe166d667f: `La synchronisation entre les appareils n'est pas activée.`,
   sf7c45e0969b403fb: `Séquence d'accords :`,
+  sf97f9b5f623b31cc: `Réessayer ?`,
 };
